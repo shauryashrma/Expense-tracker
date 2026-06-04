d714fa06a573549b418a6c265d9837f2e5dcf8b0
shauryashrma <shshshaurya12@gmail.com>
2026-06-04 12:59:52 +0530

    Update CORS origin for production deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS for frontend
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://expense-tracker-2mjg.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
